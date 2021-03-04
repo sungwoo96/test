ea4a0421342cda5adc4d0dbf7e11fda56f014725
sungwoo96 <sungwoo9671@naver.com>
2021-03-04 20:38:57 +0900

    Initial Commit

---
+++ 열혈강의 C언어 본색/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  열혈강의 C언어 본색
+//
+//  Created by Sungwoo on 2021/03/04.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
